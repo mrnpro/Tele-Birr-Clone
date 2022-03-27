cc2588ab57a283d37a3b9bdc53dae69f1a9f7de3
Natnael Adane <knatnaelaadane@gmail.com>
2022-03-27 15:04:10 +0300

    add: sizes class for constant sizes

---
+++ lib/common/sizes.dart
@@ -0,0 +1,16 @@
+class Sizes {
+  Sizes._();
+
+  static const double dimen_0 = 0;
+  static const double dimen_1 = 1;
+  static const double dimen_2 = 2;
+  static const double dimen_3 = 3;
+  static const double dimen_4 = 4;
+  static const double dimen_5 = 5;
+  static const double dimen_6 = 6;
+  static const double dimen_7 = 7;
+  static const double dimen_8 = 8;
+  static const double dimen_9 = 9;
+  static const double dimen_10 = 10;
+  static const double dimen_20 = 20;
+}
